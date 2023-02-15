eeb32aa64aa77e87d611a2eb7a45c208ef09c17f
popskully <davionlounds85@gmail.com>
2023-02-15 16:07:02 -0500

    favicon fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,31 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link rel="shortcut icon" href="/images/favicon/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon/favicon-16x16.png"
+        />
+      </Head>
       <Component {...pageProps} />
       <Analytics />
     </>
